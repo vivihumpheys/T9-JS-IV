ff60db7ae52d33358619d12ea1acbd6330ed3c36
Viviane Miranda <vivianehfm@gmail.com>
2020-06-01 16:47:58 -0300

    resolução exercicio objetos

--- revisao/objetos.js
+++ revisao/objetos.js
@@ -1,15 +1,98 @@
-const pessoa = {
-  personalidade: ['personalide1', 'personalide2'],
-  areaAtuacao: 'Biologa'
-}
+// const pessoa = {
+//   personalidade: ['personalide1', 'personalide2'],
+//   areaAtuacao: 'Biologa'
+// }
 
-pessoa.personalidade[0];
+// pessoa.personalidade[0];
 
-console.log("Personalidade1: ", pessoa.personalidade[0]);
+// console.log("Personalidade1: ", pessoa.personalidade[0]);
 
 
 //exercício
+// CLIENTES: 
+
+// 	CLIENTE1: 
+
+// 	Nome: Dora Barreto 
+// 	CPF: 223.001.221-29
+// 	Valor da compra: R$ 2.000,00
+// 	Modelo do celular: XXX-12
+// 	Ano do celular: 2020
+// 	Cor do celular: Amarelo
+// 	Loja: RJ-BR
+// 	Atendente: Joana Maria 
+
+
+// 	CLIENTE 2: 
+
+// 	Nome: Marcela Janino Silva 
+// 	CPF: 321.101.231-29
+// 	Valor da compra: R$ 1.300,00
+// 	Modelo do celular: XXM-y
+// 	Ano do celular: 2019
+// 	Cor do celular: ROXO
+// 	Loja: SP-BR
+// 	Atendente: Drielly Gomes Almeida
+
+
+// 	CLIENTE 3: 
+
+// 	Nome: Raquel Borges Figueredo
+// 	CPF: 910.311.425-01
+// 	Valor da compra: R$ 4.000,00
+// 	Modelo do celular: MDM-PRO
+// 	Ano do celular: 2020
+// 	Cor do celular: AZUL
+// 	Loja: MG-BR
+// 	Atendente: Fran Carneiro de Pinha
 
 
 
+// const clientes = {
+//   nome: ['Dora Barreto', 'Marcela Janino Silva', 'Raquel Borges Figueredo'],
+//   cpf: ['223.001.221-29', '321.101.231-29', ' 910.311.425-01'],
+//   valorCompra: [2.000,00, 1.300,00, 4.000,00 ],
+//   modeloCelular: ['XXX-12', 'XXM-y', 'MDM-PRO'],
+//   anoCelular: ['2020', '2019', '2020'],
+//   corCelular: ['Amarelo', 'ROXO', 'AZUL'],
+//   loja: ['RJ-BR', 'SP-BR', 'MG-BR'],
+//   atendente: ['Joana Maria', 'Drielly Gomes Almeida', 'Fran Carneiro de Pinha']
 
+// }
+
+const cliente1 = {
+  nome: 'Dora Barreto', 
+	cpf: '223.001.221-29',
+	valorCompra:  2.000 ,
+	modeloCelular: 'XXX-12',
+	anoCelular: '2020',
+	corCelular: 'Amarelo',
+	loja: 'RJ-BR',
+	atendente: 'Joana Maria'
+
+}
+
+const cliente2 = {
+  nome: 'Marcela Janino Silva',
+	cpf: '321.101.231-29',
+	valorCompra: 1.300,
+	modeloCelular: 'XXM-y',
+	anoCelular: '2019',
+	corCelular: 'ROXO',
+	loja: 'SP-BR',
+	atendente: 'Drielly Gomes Almeida'
+
+}
+
+const cliente3 = {
+ 	nome: 'Raquel Borges Figueredo',
+	cpf: '910.311.425-01',
+	valorCompra: 4.000,
+	modeloCelular:  'MDM-PRO',
+	anoCelular: 2020,
+  corCelular: 'AZUL',
+  loja: 'MG-BR',
+  atendente: 'Fran Carneiro de Pinha'
+}
+
+console.log (clientes)
