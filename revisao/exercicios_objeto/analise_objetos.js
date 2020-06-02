d892fa94b9203c6ce3c1bfbfb5f0fc1ec61b2e10
Viviane Miranda <vivianehfm@gmail.com>
2020-06-02 16:29:15 -0300

    exercicios orientacao obj - signos

--- revisao/exercicios_objeto/analise_objetos.js
+++ revisao/exercicios_objeto/analise_objetos.js
@@ -2,14 +2,16 @@
 //Em seguida, ajuste o objeto produtos da forma correta de se declarar.
 let produtos = {
   videos: {
-    comedia: ["comedia1""comedia2"];
-    romance ["romance1", "romance2"]
+    comedia : ["comedia1","comedia2"],
+    romance : ["romance1", "romance2"]
   },
-  revistas: [
-    moda = ["lalala", "lililili"],
-    saude = ["lalala", "lililili"],
-  ]
-  jogos {
-    rpg ["rpg1", "rpg2", rpg3],
-    acao: ["acao1", "God of War"]
+  revistas: {
+    moda : ["lalala", "lililili"],
+    saude : ["lalala", "lililili"],
+  },
+  jogos: {
+    rpg :["rpg1", "rpg2", "rpg3"],
+    acao : ["acao1", "God of War"],
   }
+}
+console.log (produtos)
---
+++ revisao/exercicios_objeto/orientacaoObjetos.js
@@ -0,0 +1,11 @@
+class Signos {
+    constructor(nome, elemento, regente) {
+        this.nome = nome;
+        this.elemento = elemento;
+        this.regente = regente;
+
+    }
+}
+
+const pessoa1 = new Signos ("virgem", "terra", "mercúrio");
+console.log (pessoa1)
